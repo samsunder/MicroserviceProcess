9f3db09f70706197387ae0c72bd948ca7265588b
Sam Sunder Golla <31933580+samsunder@users.noreply.github.com>
2021-10-30 23:41:41 -0500

    Setup Files

---
+++ ProcessService/src/test/java/com/sam/ProcessService/ProcessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.ProcessService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
